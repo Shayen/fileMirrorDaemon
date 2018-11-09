02ef868e47ad28fbae5f05c47e0dea7f15a4fbc8
nook <nook@localhost.localdomain>
2018-11-09 19:11:03 +0700

    :herb: Complete create daemon and merge diff files

--- fileMirror.py
+++ fileMirror.py
@@ -1,69 +1,351 @@
+_version__ = '0.9'
+
+import sys
 import os
 import filecmp
-import core
 import shutil
+import logging
+import logging.handlers
+import traceback
+import time
+import inspect
 
-def get_diff_files(source, dest, IGNORE_dir=[], IGNORE_fileType = []):
-	source = source.replace('\\', '/')
-	dest   = dest.replace('\\', '/')
+from src import daemon
+from src import reader
 
-	# List files
-	s_files = core.walkingDir(source,IGNORE_dir=IGNORE_dir, IGNORE_fileType = IGNORE_fileType)
-	d_files = core.walkingDir(dest, IGNORE_dir=IGNORE_dir, IGNORE_fileType = IGNORE_fileType)
+def _setFilePathOnLogger(logger, path):
+	# Remove any previous handler.
+	_removeHandlersFromLogger(logger, logging.handlers.TimedRotatingFileHandler)
 
-	# Get relative source file
-	rel_source = []
-	for f_id in range(len(s_files)):
-		rel_source.append(s_files[f_id].replace('\\', '/').replace(source, ''))
+	# Add the file handler
+	handler = logging.handlers.TimedRotatingFileHandler(path, 'midnight', backupCount=10)
+	handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+	logger.addHandler(handler)
 
-	# Get relative destination file
-	rel_dest = []
-	for f_id in range(len(d_files)) :
-		rel_dest.append( d_files[f_id].replace('\\', '/').replace(dest, '') )
+def _removeHandlersFromLogger(logger, handlerTypes=None):
+	"""
+	Remove all handlers or handlers of a specified type from a logger.
+	@param logger: The logger who's handlers should be processed.
+	@type logger: A logging.Logger object
+	@param handlerTypes: A type of handler or list/tuple of types of handlers
+		that should be removed from the logger. If I{None}, all handlers are
+		removed.
+	@type handlerTypes: L{None}, a logging.Handler subclass or
+		I{list}/I{tuple} of logging.Handler subclasses.
+	"""
+	for handler in logger.handlers:
+		if handlerTypes is None or isinstance(handler, handlerTypes):
+			logger.removeHandler(handler)
 
-	# Compare file
-	need2add = []
-	need2remove = []
-	# Find file for copy to destination
-	for rel_s_file in rel_source:
-		s_file = source + rel_s_file
-		d_file = dest + rel_s_file
+class Config(object):
 
-		cmp_result = False
-		if rel_s_file in rel_dest :
-			cmp_result = filecmp.cmp(s_file, d_file, shallow=True)
+	print "current location :", __file__
 
-		if not cmp_result :
-			# Copy file
-			if not os.path.exists(os.path.dirname(d_file)) :
-				os.makedirs(os.path.dirname(d_file))
+	def __init__(self):
+		currentPath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+		example_config_path = "%s/%s"%(currentPath, 'example_config.yml')
+		config_path = "%s/%s"%(currentPath, 'config.yml')
 
-			# print "Copy :", s_file, ">", d_file
-			need2add.append((s_file, d_file))
+		if not os.path.exists(config_path):
+			shutil.copy2(example_config_path, config_path)
 
-	# Find file that need to remove from destination
-	for rel_d_file in rel_dest:
-		# s_file = source + rel_d_file
-		d_file = dest + rel_d_file
+		self._configData = reader.read(config_path)
 
-		if rel_d_file not in rel_source:
-			print rel_d_file, d_file
-			need2remove.append(d_file)
+	@property
+	def PIDFile(self):
+		return self._configData['pidFile']
 
-	return need2add, need2remove
+	@property
+	def source(self):
+		return self._configData['source']
+
+	@property
+	def destination(self):
+		return self._configData['destination']
+
+	@property
+	def logPath(self):
+		return self._configData['logPath']
+
+	@property
+	def logFile(self):
+		return self._configData['logFile']
+
+	@property
+	def logLevel(self):
+		return int(self._configData['logLevel'])
+
+	@property
+	def logMode(self):
+		return int(self._configData['logMode'])
+
+	@property
+	def gitRepo(self):
+		return self._configData['gitRepo']
+
+	@property
+	def fetch_interval(self):
+		return int(self._configData['fetch_interval'])
+
+	@property
+	def IGNORE_dir(self):
+		result = self._configData.get('IGNORE_dir', [])
+		return  result if type(result) == type(dict()) else list(result)
+
+	@property
+	def IGNORE_fileType(self):
+		result = self._configData.get('IGNORE_fileType', [])
+		return result if type(result) == type(dict()) else list(result)
+
+	def getLogFile(self, filename=None):
+
+		if filename is None:
+			if self.logFile:
+				filename = self.logFile
+			else:
+				raise IOError('The config file has no logFile option.')
+
+		path = self.logPath
+		if path:
+
+			if not os.path.exists(path):
+				os.makedirs(path)
+			elif not os.path.isdir(path):
+				raise IOError('The logPath value in the config should point to a directory.')
+
+			path = os.path.join(path, filename)
+		else :
+			path = filename
+
+			return path
+
+class Engine(object):
+
+	def __init__(self):
+		self._continue = True
+		self.config = Config()
+
+		# Setup the logger for the main engine
+		if self.config.logMode == 0:
+			# Set the root logger for file output.
+			rootLogger = logging.getLogger()
+			# rootLogger.setLevel(self.config.logLevel)
+			_setFilePathOnLogger(rootLogger, self.config.getLogFile())
+			print 'Log file :', self.config.getLogFile()
+
+			# Set the engine logger for email output.
+			self.log = logging.getLogger('engine')
+			# self.setEmailsOnLogger(self.log, True)
+		else:
+			# Set the engine logger for file and email output.
+			self.log = logging.getLogger('engine')
+			self.log.config = self.config
+			_setFilePathOnLogger(self.log, self.config.getLogFile())
+			# self.setEmailsOnLogger(self.log, True)
+
+		self.log.setLevel(self.config.logLevel)
+
+		super(Engine, self).__init__()
+
+	def _get_diff_files(self, source, dest, IGNORE_dir=[], IGNORE_fileType=[]):
+
+		source = source.replace('\\', '/')
+		dest = dest.replace('\\', '/')
+
+		# List files
+		s_files = walkingDir(source, IGNORE_dir=IGNORE_dir, IGNORE_fileType=IGNORE_fileType)
+		d_files = walkingDir(dest, IGNORE_dir=IGNORE_dir, IGNORE_fileType=IGNORE_fileType) if os.path.exists(
+			dest) else []
+
+		# Get relative source file
+		rel_source = []
+		for f_id in range(len(s_files)):
+			rel_source.append(s_files[f_id].replace('\\', '/').replace(source, ''))
+
+		# Get relative destination file
+		rel_dest = []
+		for f_id in range(len(d_files)):
+			rel_dest.append(d_files[f_id].replace('\\', '/').replace(dest, ''))
+
+		# Compare file
+		need2add = []
+		need2remove = []
+		# Find file for copy to destination
+		for rel_s_file in rel_source:
+			s_file = source + rel_s_file
+			d_file = dest + rel_s_file
+
+			cmp_result = False
+			if rel_s_file in rel_dest:
+				cmp_result = filecmp.cmp(s_file, d_file, shallow=True)
+
+			if not cmp_result:
+				# Copy file
+				if not os.path.exists(os.path.dirname(d_file)):
+					os.makedirs(os.path.dirname(d_file))
+
+				# print "Copy :", s_file, ">", d_file
+				need2add.append((s_file, d_file))
+
+		# Find file that need to remove from destination
+		for rel_d_file in rel_dest:
+			# s_file = source + rel_d_file
+			d_file = dest + rel_d_file
+
+			if rel_d_file not in rel_source:
+				need2remove.append(d_file)
+
+		print "compare finished"
+		return need2add, need2remove
+
+	def _mirror(self):
+
+		self.log.debug("Getting difference files ...")
+
+		need2add, need2remove = self._get_diff_files(self.config.source, self.config.destination,
+								                     IGNORE_dir=self.config.IGNORE_dir,
+								                     IGNORE_fileType=self.config.IGNORE_fileType)
+
+		if need2add == [] and need2remove == [] :
+			self.log.debug("Files is indenticald.")
+		else:
+			for _s, _d in need2add:
+				self.log.debug("Copy : %s -> %s"%(_s,_d))
+				shutil.copy2(_s, _d)
+
+			for _f in need2remove :
+				self.log.debug("Remove : %s"%(_f))
+				os.remove(_f)
+
+			self.log.debug("Mirror success.")
+
+	def start(self):
+		try:
+			self._mainLoop()
+		except KeyboardInterrupt:
+			self.log.warning('Keyboard interrupt. Cleaning up...')
+		except Exception, err:
+			msg = 'Crash!!!!! Unexpected error (%s) in main loop.\n\n%s'
+			self.log.critical(msg, type(err), traceback.format_exc(err))
+
+	def stop(self):
+		self._continue = False
+
+	def _mainLoop(self):
+		self.log.debug("Main loop started.")
+		while self._continue:
+			self._mirror()
+			time.sleep(self.config.fetch_interval)
+
+		self.log.debug('Shuting down processing loop.')
+
+class LinuxDaemon(daemon.Daemon):
+
+	def __init__(self):
+		self._engine = Engine()
+		super(LinuxDaemon, self).__init__( serviceName= 'fileMirror',pidfile = self._engine.config.PIDFile)
+
+	def start(self, daemonize=True):
+
+		if not daemonize:
+			# Setup the stdout logger
+			handler = logging.StreamHandler()
+			handler.setFormatter(logging.Formatter("%(levelname)s:%(name)s:%(message)s"))
+			logging.getLogger().addHandler(handler)
+
+		super(LinuxDaemon, self).start(daemonize)
+
+	def _run(self):
+		self._engine.start()
+
+	def _cleanup(self):
+		self._engine.stop()
+
+def walkingDir(src, IGNORE_dir=[], IGNORE_fileType = []):
+
+	files = []
+	dirs = []
+
+	if os.path.isfile(src):
+		return [src]
+
+	first_list = [src + '/' + i for i in os.listdir(src)]
+	for file in first_list:
+		if os.path.isdir(file):
+			dirs.append(file)
+
+		else:
+			files.append(file)
+
+	# Loop to get all sub files and folders.
+	dirs_temp = dirs[:]
+	while (len(dirs_temp) > 0):
+		i = dirs_temp.pop(0)
+
+		subfolder = [i + '/' + j for j in os.listdir(i) if os.path.isdir(i + '/' + j)]
+		subfiles = [i + '/' + j for j in os.listdir(i) if os.path.isfile(i + '/' + j)]
+
+		dirs = dirs + subfolder
+		files = files + subfiles
+
+		dirs_temp = dirs_temp + subfolder
+
+	# Exclude ignore files and folders.
+	exclude_dir = []
+	for folder in dirs:
+		if os.path.basename(folder) in IGNORE_dir:
+			exclude_dir.append(folder)
+
+	for file in files[:]:
+		if os.path.splitext(file)[1] in IGNORE_fileType:
+			files.remove(file)
+
+		for each in exclude_dir:
+			if each in file:
+				files.remove(file)
+
+	return files
 
 def main ():
-	source = "H:/programming/Python/fileMirrorDaemon"
-	dest = 'C:/Users/siras/Downloads/dest/fileMirrorDaemon'
-	need2add, need2remove = get_diff_files(source, dest,IGNORE_dir=[".idea"], IGNORE_fileType = ['.pyc'])
 
-	print "Need to add"
-	for i in need2add :
-		print '\t' , i[0], '>', i[1]
+	if sys.platform not in ("linux", "linux2"):
+		print "Support only Linux."
+		return 2
 
-	print "Need to remove"
-	for j in need2remove :
-		print '\t' , j
+	action = None
+
+	if len(sys.argv) > 1:
+		action = sys.argv[1]
+
+	if action:
+		daemon = LinuxDaemon()
+		func = getattr(daemon, action, None)
+		if action[:1] != '_' and func != None :
+			func()
+			return 0
+
+	print "usage: %s start|stop|restart|foreground" % sys.argv[0]
+	return 2
+
+# source = "/mnt/META/SCRIPTS/MEME"
+	# dest = os.environ['HOME']+'/META/SCRIPTS/MEME'
+	# need2add, need2remove = get_diff_files(source, dest,IGNORE_dir=[".idea", 'USER_ACTIVITY'], IGNORE_fileType = ['.pyc'])
+	#
+	# print "Need to add"
+	# for i in need2add :
+	# 	# print '\t' , i[0], '>', i[1]
+	# 	print ( '\t' + i[0] + '>'+ i[1])
+	# 	shutil.copy2(i[0], i[1])
+	#
+	# print "Need to remove"
+	# for j in need2remove :
+	# 	print '\t' , j
+	# 	os.remove(j)
 
 if __name__ == '__main__':
-	main()
+	sys.exit(main())
+	# import tqdm, time
+	#
+	# for i in tqdm.tqdm(range(10)):
+	# 	time.sleep(0.2)
+
--- src/daemon.py
+++ src/daemon.py
@@ -1,249 +1,174 @@
-'''
-***
-Modified generic daemon class
-***
+#!/usr/bin/env python
 
-Author:         http://www.jejik.com/articles/2007/02/
-                        a_simple_unix_linux_daemon_in_python/www.boxedice.com
+# Taken and modified from:
+# http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
 
-License:        http://creativecommons.org/licenses/by-sa/3.0/
-
-Changes:        23rd Jan 2009 (David Mytton <david@boxedice.com>)
-                - Replaced hard coded '/dev/null in __init__ with os.devnull
-                - Added OS check to conditionally remove code that doesn't
-                  work on OS X
-                - Added output to console on completion
-                - Tidied up formatting
-                11th Mar 2009 (David Mytton <david@boxedice.com>)
-                - Fixed problem with daemon exiting on Python 2.4
-                  (before SystemExit was part of the Exception base)
-                13th Aug 2010 (David Mytton <david@boxedice.com>
-                - Fixed unhandled exception if PID file is empty
-'''
-
-# Core modules
-from __future__ import print_function
 import atexit
-import errno
 import os
+import signal
 import sys
 import time
-import signal
+
+
+if (hasattr(os, "devnull")):
+    DEVNULL = os.devnull
+else:
+    DEVNULL = "/dev/null"
 
 
 class Daemon(object):
     """
     A generic daemon class.
 
-    Usage: subclass the Daemon class and override the run() method
+    Usage: subclass the Daemon class and override the _run() method
     """
-    def __init__(self, pidfile, stdin=os.devnull,
-                 stdout=os.devnull, stderr=os.devnull,
-                 home_dir='.', umask=0o22, verbose=1,
-                 use_gevent=False, use_eventlet=False):
-        self.stdin = stdin
-        self.stdout = stdout
-        self.stderr = stderr
-        self.pidfile = pidfile
-        self.home_dir = home_dir
-        self.verbose = verbose
-        self.umask = umask
-        self.daemon_alive = True
-        self.use_gevent = use_gevent
-        self.use_eventlet = use_eventlet
+    def __init__(self, serviceName, pidfile, stdin=DEVNULL, stdout=DEVNULL, stderr=DEVNULL):
+        super(Daemon, self).__init__()
 
-    def log(self, *args):
-        if self.verbose >= 1:
-            print(*args)
+        self._serviceName = serviceName
+        self._stdin = stdin
+        self._stdout = stdout
+        self._stderr = stderr
+        self._pidfile = pidfile
 
-    def daemonize(self):
+    def _daemonize(self):
         """
         Do the UNIX double-fork magic, see Stevens' "Advanced
         Programming in the UNIX Environment" for details (ISBN 0201563177)
         http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
         """
-        if self.use_eventlet:
-            import eventlet.tpool
-            eventlet.tpool.killall()
         try:
             pid = os.fork()
             if pid > 0:
-                # Exit first parent
+                # exit first parent
                 sys.exit(0)
-        except OSError as e:
-            sys.stderr.write(
-                "fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+        except OSError, e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
             sys.exit(1)
 
-        # Decouple from parent environment
-        os.chdir(self.home_dir)
+        # decouple from parent environment
+        os.chdir("/")
         os.setsid()
-        os.umask(self.umask)
+        os.umask(0)
 
-        # Do second fork
+        # do second fork
         try:
             pid = os.fork()
             if pid > 0:
-                # Exit from second parent
+                # exit from second parent
                 sys.exit(0)
-        except OSError as e:
-            sys.stderr.write(
-                "fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+        except OSError, e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
             sys.exit(1)
 
-        if sys.platform != 'darwin':  # This block breaks on OS X
-            # Redirect standard file descriptors
-            sys.stdout.flush()
-            sys.stderr.flush()
-            si = open(self.stdin, 'r')
-            so = open(self.stdout, 'a+')
-            if self.stderr:
-                try:
-                    se = open(self.stderr, 'a+', 0)
-                except ValueError:
-                    # Python 3 can't have unbuffered text I/O
-                    se = open(self.stderr, 'a+', 1)
-            else:
-                se = so
-            os.dup2(si.fileno(), sys.stdin.fileno())
-            os.dup2(so.fileno(), sys.stdout.fileno())
-            os.dup2(se.fileno(), sys.stderr.fileno())
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self._stdin, 'r')
+        so = file(self._stdout, 'a+')
+        se = file(self._stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
 
-        def sigtermhandler(signum, frame):
-            self.daemon_alive = False
-            sys.exit()
-
-        if self.use_gevent:
-            import gevent
-            gevent.reinit()
-            gevent.signal(signal.SIGTERM, sigtermhandler, signal.SIGTERM, None)
-            gevent.signal(signal.SIGINT, sigtermhandler, signal.SIGINT, None)
-        else:
-            signal.signal(signal.SIGTERM, sigtermhandler)
-            signal.signal(signal.SIGINT, sigtermhandler)
-
-        self.log("Started")
-
-        # Write pidfile
-        atexit.register(
-            self.delpid)  # Make sure pid file is removed if we quit
+        # write pidfile and subsys file
         pid = str(os.getpid())
-        open(self.pidfile, 'w+').write("%s\n" % pid)
+        file(self._pidfile,'w+').write("%s\n" % pid)
+        if os.path.exists('/var/lock/subsys'):
+            fh = open(os.path.join('/var/lock/subsys', self._serviceName), 'w')
+            fh.close()
 
-    def delpid(self):
-        try:
-            # the process may fork itself again
-            pid = int(open(self.pidfile, 'r').read().strip())
-            if pid == os.getpid():
-                os.remove(self.pidfile)
-        except OSError as e:
-            if e.errno == errno.ENOENT:
-                pass
-            else:
-                raise
+    def _delpid(self):
+        if os.path.exists(self._pidfile):
+            os.remove(self._pidfile)
 
-    def start(self, *args, **kwargs):
+        subsysPath = os.path.join('/var/lock/subsys', self._serviceName)
+        if os.path.exists(subsysPath):
+            os.remove(subsysPath)
+
+        self._cleanup()
+
+    def start(self, daemonize=True):
         """
         Start the daemon
         """
-
-        self.log("Starting...")
-
         # Check for a pidfile to see if the daemon already runs
         try:
-            pf = open(self.pidfile, 'r')
+            pf = file(self._pidfile,'r')
             pid = int(pf.read().strip())
             pf.close()
         except IOError:
             pid = None
-        except SystemExit:
-            pid = None
 
         if pid:
-            message = "pidfile %s already exists. Is it already running?\n"
-            sys.stderr.write(message % self.pidfile)
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self._pidfile)
             sys.exit(1)
 
         # Start the daemon
-        self.daemonize()
-        self.run(*args, **kwargs)
+        if daemonize:
+            self._daemonize()
+
+        # Cleanup handling
+        def termHandler(signum, frame):
+            self._delpid()
+        signal.signal(signal.SIGTERM, termHandler)
+        atexit.register(self._delpid)
+
+        # Run the daemon
+        self._run()
 
     def stop(self):
         """
         Stop the daemon
         """
-
-        if self.verbose >= 1:
-            self.log("Stopping...")
-
         # Get the pid from the pidfile
-        pid = self.get_pid()
-
-        if not pid:
-            message = "pidfile %s does not exist. Not running?\n"
-            sys.stderr.write(message % self.pidfile)
-
-            # Just to be sure. A ValueError might occur if the PID file is
-            # empty but does actually exist
-            if os.path.exists(self.pidfile):
-                os.remove(self.pidfile)
-
-            return  # Not an error in a restart
-
-        # Try killing the daemon process
         try:
-            i = 0
-            while 1:
-                os.kill(pid, signal.SIGTERM)
-                time.sleep(0.1)
-                i = i + 1
-                if i % 10 == 0:
-                    os.kill(pid, signal.SIGHUP)
-        except OSError as err:
-            if err.errno == errno.ESRCH:
-                if os.path.exists(self.pidfile):
-                    os.remove(self.pidfile)
-            else:
-                print(str(err))
-                sys.exit(1)
-
-        self.log("Stopped")
-
-    def restart(self):
-        """
-        Restart the daemon
-        """
-        self.stop()
-        self.start()
-
-    def get_pid(self):
-        try:
-            pf = open(self.pidfile, 'r')
+            pf = file(self._pidfile,'r')
             pid = int(pf.read().strip())
             pf.close()
         except IOError:
             pid = None
-        except SystemExit:
-            pid = None
-        return pid
 
-    def is_running(self):
-        pid = self.get_pid()
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self._pidfile)
+            return # not an error in a restart
 
-        if pid is None:
-            self.log('Process is stopped')
-            return False
-        elif os.path.exists('/proc/%d' % pid):
-            self.log('Process (pid %d) is running...' % pid)
-            return True
-        else:
-            self.log('Process (pid %d) is killed' % pid)
-            return False
+        # Try killing the daemon process
+        try:
+            while 1:
+                os.kill(pid, signal.SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self._pidfile):
+                    os.remove(self._pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
 
-    def run(self):
+    def foreground(self):
+        self.start(daemonize=False)
+
+    def restart(self, daemonize=True):
         """
-        You should override this method when you subclass Daemon.
-        It will be called after the process has been
-        daemonized by start() or restart().
+        Restart the daemon
         """
-        raise NotImplementedError
+        self.stop()
+        self.start(daemonize)
+
+    def _run(self):
+        """
+        You should override this method when you subclass Daemon. It will be
+        called after the process has been daemonized by start() or restart().
+        """
+        raise NotImplementedError('You must implement the method in your class.')
+
+    def _cleanup(self):
+        """
+        You should override this method when you subclass Daemon. It will be
+        called when the daemon exits.
+        """
+        raise NotImplementedError('You must implement the method in your class.')
